28053585d3be747553f62534adc0ae1f43576fe0
Casey Joiner <caseyejoiner@gmail.com>
2025-05-23 10:17:37 -0500

    Add hero section to home page, update tea index template, and improve CSS styles

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hello tea lover!</h1>')  
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')  
@@ -16,10 +16,10 @@ class Tea:
         self.image = image
         
 teas = [
-    Tea('2024 Nightlife', 'Heavy, creamy mouthfeel with an airy sweetness and aroma', 'Moonlight white', 'Yunnan, China', ),
-    Tea('2016 Poundcake', 'A blend of raw Puer material that has strength, medium light bitterness, and sweetness', 'Raw Puer', 'Yunnan, China'),
-    Tea('Duckshit on the Breeze', 'Elegant and wisy take on the classic Duckshit tea', 'Dancong Oolong', 'Guangdong Province, China'),
-    Tea('Fruit Bomb Lapsang', 'Heavy, intense fruit fragrances over a full bodied, smoky black tea', 'Lapsang Souchong', 'Fujian Province, China')
+    Tea('2024 Nightlife', 'Heavy, creamy mouthfeel with an airy sweetness and aroma', 'Moonlight white', 'Yunnan, China', 'images/2024Nightlife.png' ),
+    Tea('2016 Poundcake', 'A blend of raw Puer material that has strength, medium light bitterness, and sweetness', 'Raw Puer', 'Yunnan, China', 'images/poundcake.png'),
+    Tea('Duckshit on the Breeze', 'Elegant and wisy take on the classic Duckshit tea', 'Dancong Oolong', 'Guangdong Province, China', 'images/duckshit.png'),
+    Tea('Fruit Bomb Lapsang', 'Heavy, intense fruit fragrances over a full bodied, smoky black tea', 'Lapsang Souchong', 'Fujian Province, China', 'images/FruitBombLapsang.png'),
 ]
 
 def tea_index(request):
