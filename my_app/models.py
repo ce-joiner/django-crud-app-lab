6a8c426310316006b4b51519e3637930c23565cd
Casey Joiner <caseyejoiner@gmail.com>
2025-05-23 11:56:23 -0500

    Add Tea model, migration, and update views for tea index, added pillow for images

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Tea(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    type = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Tea
 # from django.http import HttpResponse
 
 def home(request):
@@ -7,21 +8,21 @@ def home(request):
 def about(request):
     return render(request, 'about.html')  
 
-class Tea:
-    def __init__(self, name, description, type, origin, image):
-        self.name = name
-        self.description = description
-        self.type = type
-        self.origin = origin
-        self.image = image
+# class Tea:
+#     def __init__(self, name, description, type, origin, image):
+#         self.name = name
+#         self.description = description
+#         self.type = type
+#         self.origin = origin
+#         self.image = image
         
-teas = [
-    Tea('2024 Nightlife', 'Heavy, creamy mouthfeel with an airy sweetness and aroma', 'Moonlight white', 'Yunnan, China', 'images/2024Nightlife.png' ),
-    Tea('2016 Poundcake', 'A blend of raw Puer material that has strength, medium light bitterness, and sweetness', 'Raw Puer', 'Yunnan, China', 'images/poundcake.png'),
-    Tea('Duckshit on the Breeze', 'Elegant and wisy take on the classic Duckshit tea', 'Dancong Oolong', 'Guangdong Province, China', 'images/duckshit.png'),
-    Tea('Fruit Bomb Lapsang', 'Heavy, intense fruit fragrances over a full bodied, smoky black tea', 'Lapsang Souchong', 'Fujian Province, China', 'images/FruitBombLapsang.png'),
-]
+# teas = [
+#     Tea('2024 Nightlife', 'Heavy, creamy mouthfeel with an airy sweetness and aroma', 'Moonlight white', 'Yunnan, China', 'images/2024Nightlife.png' ),
+#     Tea('2016 Poundcake', 'A blend of raw Puer material that has strength, medium light bitterness, and sweetness', 'Raw Puer', 'Yunnan, China', 'images/poundcake.png'),
+#     Tea('Duckshit on the Breeze', 'Elegant and wisy take on the classic Duckshit tea', 'Dancong Oolong', 'Guangdong Province, China', 'images/duckshit.png'),
+#     Tea('Fruit Bomb Lapsang', 'Heavy, intense fruit fragrances over a full bodied, smoky black tea', 'Lapsang Souchong', 'Fujian Province, China', 'images/FruitBombLapsang.png'),
+# ]
 
 def tea_index(request):
-    # render the tea_index.html template with the teas list
+    teas = Tea.objects.all()  # Fetch all tea objects from the database
     return render(request, 'teas/index.html', {'teas': teas})
