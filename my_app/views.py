7512d0231a328423cd9788c71b039d8b19240cfb
Casey Joiner <caseyejoiner@gmail.com>
2025-05-23 08:29:08 -0500

    Add tea index page, card styles, and tea data model

--- my_app/views.py
+++ my_app/views.py
@@ -7,3 +7,21 @@ def home(request):
 def about(request):
     return render(request, 'about.html')  
 
+class Tea:
+    def __init__(self, name, description, type, origin, image):
+        self.name = name
+        self.description = description
+        self.type = type
+        self.origin = origin
+        self.image = image
+        
+teas = [
+    Tea('2024 Nightlife', 'Heavy, creamy mouthfeel with an airy sweetness and aroma', 'Moonlight white', 'Yunnan, China', ),
+    Tea('2016 Poundcake', 'A blend of raw Puer material that has strength, medium light bitterness, and sweetness', 'Raw Puer', 'Yunnan, China'),
+    Tea('Duckshit on the Breeze', 'Elegant and wisy take on the classic Duckshit tea', 'Dancong Oolong', 'Guangdong Province, China'),
+    Tea('Fruit Bomb Lapsang', 'Heavy, intense fruit fragrances over a full bodied, smoky black tea', 'Lapsang Souchong', 'Fujian Province, China')
+]
+
+def tea_index(request):
+    # render the tea_index.html template with the teas list
+    return render(request, 'teas/index.html', {'teas': teas})
